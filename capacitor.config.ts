f8dabc84904025c618d6efcee0eda2185bbd98c8
sh3ki <shekaigarcia@gmail.com>
2026-09-02 07:43:50 +0800

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'online.yunafied.app',
+  appName: 'YUNAFied',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
