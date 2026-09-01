14814c4f950a0ed387837634ec8fdfe46d553932
sh3ki <shekaigarcia@gmail.com>
2026-09-02 00:21:53 +0800

    Update backend/src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -656,6 +656,10 @@ async function uploadDocumentBufferToCloudinary(file: Express.Multer.File): Prom
     : [
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:8081",
+        "http://127.0.0.1:8081",
+        "http://localhost:19006",
+        "http://127.0.0.1:19006",
         "https://www.yunafied.online",
       ];
 
